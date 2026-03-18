df84fb1764032c7383e2788f3c0c1d8624f417c9
pranishkarki20 <praniskkarki20@gmail.com>
2026-03-18 12:09:24 +0530

    git push

--- ems.java
+++ ems.java
@@ -57,48 +57,59 @@ class Manager extends Employee {
 public class ems {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Employee b1 = new Employee();
-        System.out.println("Enter the name of the employee");
-        String name = sc.nextLine();
-        System.out.println("Enter the age ");
-        int age = sc.nextInt();
-        System.out.println("Enter the Salarly");
-        float salarly = sc.nextFloat();
-        int id = (int) (Math.random()*100)  + 1;
-        b1.setinfo(name);
-        b1.setage(age);
-        b1.setsalarly(salarly);
-        b1.setid(id);
-        System.out.println("The name of the employee is " + b1.getinfo());
-        System.out.println("The age of the employee is " + b1.getage());
-        System.out.println("The salary of the employee is " + b1.getsalarly());
-        System.out.println(b1.getinfo()+ " id will be " + b1.getid());
-
-        // Manager section (inherits Employee)
+        System.out.println("Select role:");
+        System.out.println("1. Manager");
+        System.out.println("2. Worker");
+        System.out.print("Enter choice: ");
+        int choice = sc.nextInt();
         sc.nextLine();
-        Manager m1 = new Manager();
-        System.out.println("\nEnter the name of the manager");
-        String managerName = sc.nextLine();
-        System.out.println("Enter the age");
-        int managerAge = sc.nextInt();
-        System.out.println("Enter the salary");
-        float managerSalary = sc.nextFloat();
-        sc.nextLine();
-        System.out.println("Enter the department");
-        String department = sc.nextLine();
-        int managerId = (int) (Math.random()*100) + 101;
 
-        m1.setinfo(managerName);
-        m1.setage(managerAge);
-        m1.setsalarly(managerSalary);
-        m1.setid(managerId);
-        m1.setDepartment(department);
+        if (choice == 1) {
+            Manager m1 = new Manager();
+            System.out.println("\nEnter the name of the manager");
+            String managerName = sc.nextLine();
+            System.out.println("Enter the age");
+            int managerAge = sc.nextInt();
+            System.out.println("Enter the salary");
+            float managerSalary = sc.nextFloat();
+            sc.nextLine();
+            System.out.println("Enter the department");
+            String department = sc.nextLine();
+            int managerId = (int) (Math.random()*100) + 101;
 
-        System.out.println("Manager name: " + m1.getinfo());
-        System.out.println("Manager age: " + m1.getage());
-        System.out.println("Manager salary: " + m1.getsalarly());
-        System.out.println("Manager id: " + m1.getid());
-        System.out.println("Manager department: " + m1.getDepartment());
+            m1.setinfo(managerName);
+            m1.setage(managerAge);
+            m1.setsalarly(managerSalary);
+            m1.setid(managerId);
+            m1.setDepartment(department);
+
+            System.out.println("Manager name: " + m1.getinfo());
+            System.out.println("Manager age: " + m1.getage());
+            System.out.println("Manager salary: " + m1.getsalarly());
+            System.out.println("Manager id: " + m1.getid());
+            System.out.println("Manager department: " + m1.getDepartment());
+        } else if (choice == 2) {
+            Employee b1 = new Employee();
+            System.out.println("\nEnter the name of the worker");
+            String name = sc.nextLine();
+            System.out.println("Enter the age");
+            int age = sc.nextInt();
+            System.out.println("Enter the salary");
+            float salarly = sc.nextFloat();
+            int id = (int) (Math.random()*100)  + 1;
+
+            b1.setinfo(name);
+            b1.setage(age);
+            b1.setsalarly(salarly);
+            b1.setid(id);
+
+            System.out.println("Worker name: " + b1.getinfo());
+            System.out.println("Worker age: " + b1.getage());
+            System.out.println("Worker salary: " + b1.getsalarly());
+            System.out.println("Worker id: " + b1.getid());
+        } else {
+            System.out.println("Invalid choice.");
+        }
     }
     
 }
